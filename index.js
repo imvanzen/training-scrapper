30345fcb6402bcb67ad7a37653bae5e9e07831bc
J.Reczko <reczko.jakub@gmail.com>
2022-07-18 15:35:16 +0200

    Some progress

--- index.js
+++ index.js
@@ -4,6 +4,13 @@ require('dotenv').config();
 
 const SOURCE_WEBSITE = process.env.SOURCE_WEBSITE;
 const WEBFLOW_API_TOKEN = process.env.WEBFLOW_API_TOKEN;
+const WEBFLOW_WEBSITE_ID = process.env.WEBFLOW_WEBSITE_ID;
+const WEBFLOW_TRAINING_COLLECTION_ID = process.env.WEBFLOW_TRAINING_COLLECTION_ID;
+const WEBFLOW_CATEGORIES_COLLECTION_ID = process.env.WEBFLOW_CATEGORIES_COLLECTION_ID;
+
+const webflow = new Webflow({
+    token: WEBFLOW_API_TOKEN,
+})
 
 async function scrape() {
     const browser = await puppeteer.launch({})
@@ -38,15 +45,34 @@ async function scrape() {
 }
 
 async function update(data) {
-    const webflow = new Webflow({
-        token: WEBFLOW_API_TOKEN,
-    })
-    webflow.info().then((info) => console.log(info))
+    const response = await webflow.get(`/collections/${WEBFLOW_TRAINING_COLLECTION_ID}/items`)
+    console.log(`/sites/${WEBFLOW_WEBSITE_ID}/collections %o`, response)
+    return
 }
 
 async function run() {
-    const scrapedSet = await scrape()
-    await update(scrapedSet)
+    // const scrapedSet = await scrape()
+    await update()
+
+    // Source data
+    // - Scrape the content
+    // - Pull values from content
+    // - Sanitize content
+
+    // Modify collection
+    // - Pull collection items from website
+    // - Match collection items by key
+    // - Filter new collection items
+    // - Filter collection items to update
+    // - Filter collection items to archive
+    // - Add new collection items
+    // - Update existing collection items
+    // - Archive expired or not existing collections
 }
 
-run()
+try {
+    run()
+} catch (error) {
+    console.error('Unexpected error %s, %j', error, error)
+} finally {
+}
